17bccede62e44aa1b74d1b71ea9d61f25911267d
Micheal Egan <102835262+Eldiablo410@users.noreply.github.com>
2023-02-24 04:39:55 +0000

    Added process.php file to process registraation form

---
+++ process.php
@@ -0,0 +1,42 @@
+<?php
+
+//Connect to database
+$serverName = "sql9.freemysqlhosting.net";
+$userName = "sql9600663";
+$passWord = "mysql_pass_23!";
+$dbName = "sql9600663";
+
+$hashedPass = hash('sha256',$passWord);
+
+$con = new mysqli($serverName, $userName, $hashedPass, $dbName);
+
+if($con -> connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//Get the form data
+$username = $_POST["userName"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+//Insert data into Database
+$sql = "INSERT INTO Registration(userName, eMail, passWord) VALUES ('$username', '$email', '$password')";
+
+if($con->query($sql) === TRUE) {
+  //successful query
+  //Send confirmation email to the user
+  $to = $email
+  $subject = "Registration Confirmation"
+  $message = "Thank you for registering!";
+  $headers = "From: yourname@randomwebsite.com";
+
+  mail($to, $subject, $message, $headers);
+
+  //redirect to confirmation page
+  header("Location: confirmation.php");
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$con->close();
+?>
