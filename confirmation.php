bb1af1f3d1efb671660e21592a9cf63647c21eb5
Micheal Egan <102835262+Eldiablo410@users.noreply.github.com>
2023-02-24 04:47:01 +0000

    brought back index.html, added new logic for it, making index.html the "homepage"

---
+++ confirmation.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Registration Confirmation!</title>
+  </head>
+  <body>
+    <h1>Registration Complete!</h1>
+    <p>Thank you for registering. An email has been send to the email
+    you provided.</p>
+    <p><a href="registration.php">Back to registration Page</a></p>
+  </body>
+</html>
